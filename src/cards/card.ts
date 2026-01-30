2978613bf990701dbc49f90c1c0d612c742f77b9
Hembert <hembertdorian@gmail.com>
2026-01-30 12:58:47 +0100

    first commit & get successful

---
+++ src/cards/card.ts
@@ -0,0 +1,20 @@
+import {Request, Response, Router} from 'express'
+import {prisma} from "../database";
+import { CardModel } from '../generated/prisma/models';
+
+export const cardRouter = Router()
+
+// GET api/cards
+cardRouter.get('/', async (req: Request, res: Response) => {
+    try {
+        const cards: CardModel[] = await prisma.card.findMany({
+            orderBy: {
+                pokedexNumber: 'asc',
+            },
+        })
+        return res.status(200).json(cards)
+    } catch (error) {
+        console.error('Erreur lors de la récupération des cartes:', error)
+        return res.status(500).json({error: 'Erreur serveur'})
+    }
+})
